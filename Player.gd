b3d563a2679efd5ce5d250e278d60e2228e11c1f
Maciej Piatek <magic@2fd5.com>
2022-04-11 16:48:35 +0100

    fix: jump input was read twice

--- Player.gd
+++ Player.gd
@@ -16,9 +16,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
 
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y += jumpAmount
-
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	velocity.y += gravity * delta
